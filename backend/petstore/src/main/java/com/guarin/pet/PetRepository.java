7aebe79bc706d97a5f641fa749435bd1f1258f5f
shawnyy18 <151753378+shawnyy18@users.noreply.github.com>
2025-05-15 07:55:32 +0800

    Delete backend/petstore directory

--- backend/petstore/src/main/java/com/guarin/pet/PetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.guarin.pet;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetRepository extends JpaRepository<Pet, Integer> {
-}
